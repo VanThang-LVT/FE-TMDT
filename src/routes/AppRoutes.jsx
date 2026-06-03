096248c32769a6cae828936cf89fe03ed6ac50b8
Luong Van Thang <DH52201436@student.stu.edu.vn>
2026-06-03 10:47:34 +0700

    Tich hop cac router moi vao AppRoutes

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,8 +2,12 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from '../pages/auth/LoginPage';
 import RegisterPage from '../pages/auth/RegisterPage';
 import AdminPage from '../pages/admin/AdminPage';
-import CustomerPage from '../pages/customer/CustomerPage';
+import AdminCategoryPage from '../pages/admin/AdminCategoryPage';
+import AdminProductsPage from '../pages/admin/AdminProductsPage';
+import ProfilePage from '../pages/customer/ProfilePage';
+import ShopRegistrationPage from '../pages/customer/ShopRegistrationPage';
 import SellerPage from '../pages/seller/SellerPage';
+import SellerProductsPage from '../pages/seller/SellerProductsPage';
 import HomePage from '../pages/home/HomePage';
 
 function AppRoutes() {
@@ -13,9 +17,15 @@ function AppRoutes() {
 
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+      
+      {/* Admin Routes */}
       <Route path="/admin" element={<AdminPage />} />
-      <Route path="/customer" element={<CustomerPage />} />
+      <Route path="/admin/categories" element={<AdminCategoryPage />} />
+      <Route path="/admin/products" element={<AdminProductsPage />} />
+      <Route path="/profile" element={<ProfilePage />} />
+      <Route path="/register-shop" element={<ShopRegistrationPage />} />
       <Route path="/seller" element={<SellerPage />} />
+      <Route path="/seller/products" element={<SellerProductsPage />} />
 
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
